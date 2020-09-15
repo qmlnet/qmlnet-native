972dbaec0f6dadc67adeb01134ff92086e8a0259
devmil <info@devmil.de>
2020-09-15 22:13:37 +0200

    QmlNetPaintedItem: first step

---
+++ QmlNet/controls/QmlNetPaintedItem.cpp
@@ -0,0 +1,68 @@
+#include "QmlNetPaintedItem.h"
+#include <QPainter>
+#include <stdexcept>
+
+QmlNetPaintedItemBase::QmlNetPaintedItemBase(QSharedPointer<NetReference> netReference, QQuickItem *parent)
+    : QQuickPaintedItem(parent),
+      m_netReference(netReference) {
+}
+
+QmlNetPaintedItemBase::~QmlNetPaintedItemBase() {
+}
+
+bool QmlNetPaintedItemBase::supportsTextInput() const {
+    return m_supportsTextInput;
+}
+
+void QmlNetPaintedItemBase::setSupportsTextInput(bool supportsTextInput) {
+    m_supportsTextInput = supportsTextInput;
+    setFlag(ItemAcceptsInputMethod, supportsTextInput);
+}
+
+bool QmlNetPaintedItemBase::preeditActive() {
+    return m_preeditActive;
+}
+
+QString QmlNetPaintedItemBase::preeditText() {
+    return m_preeditText;
+}
+
+//input method support
+void QmlNetPaintedItemBase::inputMethodEvent(QInputMethodEvent *e) {
+    auto preeditShouldBeActive = !e->preeditString().isEmpty();
+    bool hasBeenCommited = false;
+
+    if(m_preeditActive != preeditShouldBeActive) {
+        m_preeditActive = preeditShouldBeActive;
+        Q_EMIT preeditActiveChanged();
+        hasBeenCommited = true;
+    }
+    if(m_preeditText != e->preeditString()) {
+        m_preeditText = e->preeditString();
+        Q_EMIT preeditTextChanged();
+    }
+    if(hasBeenCommited && !e->commitString().isEmpty()) {
+        QKeyEvent* event = new QKeyEvent(QKeyEvent::KeyPress,
+                                         Qt::Key_unknown,
+                                         Qt::NoModifier,
+                                         e->commitString());
+        QCoreApplication::postEvent(this,event);
+    }
+    QQuickItem::inputMethodEvent(e);
+}
+
+QVariant QmlNetPaintedItemBase::inputMethodQuery(Qt::InputMethodQuery property) const  {
+    return QQuickItem::inputMethodQuery(property);
+}
+
+void QmlNetPaintedItemBase::paint(QPainter *painter) {
+    qDebug() << "Paint";
+}
+
+extern "C" {
+
+Q_DECL_EXPORT void qqmlnetpainteditembase_update(QmlNetPaintedItemBase* paintedItem) {
+    paintedItem->update();
+}
+
+}
---
+++ QmlNet/controls/QmlNetPaintedItem.h
@@ -0,0 +1,72 @@
+#ifndef QMLNETPAINTEDITEM_H
+#define QMLNETPAINTEDITEM_H
+
+#include <QtQuick/QQuickPaintedItem>
+#include <QObject>
+#include <QmlNet/types/NetTypeInfo.h>
+#include <QmlNet.h>
+#include <QmlNet/types/Callbacks.h>
+
+class QmlNetPaintedItemBase : public QQuickPaintedItem {
+    Q_OBJECT
+
+    Q_PROPERTY(bool supportsTextInput READ supportsTextInput WRITE setSupportsTextInput)
+    Q_PROPERTY(bool preeditActive READ preeditActive NOTIFY preeditActiveChanged)
+    Q_PROPERTY(QString preeditText READ preeditText NOTIFY preeditTextChanged)
+
+public:
+    QmlNetPaintedItemBase(QSharedPointer<NetReference> netReference, QQuickItem *parent = nullptr);
+
+    virtual ~QmlNetPaintedItemBase();
+
+    bool supportsTextInput() const;
+
+    void setSupportsTextInput(bool supportsTextInput);
+
+    bool preeditActive();
+
+    Q_SIGNAL void preeditActiveChanged();
+
+    QString preeditText();
+
+    Q_SIGNAL void preeditTextChanged();
+
+    //input method support
+    void inputMethodEvent(QInputMethodEvent *e) override;
+
+    QVariant inputMethodQuery(Qt::InputMethodQuery property) const override;
+
+    void paint(QPainter *painter) override;
+
+private:
+    bool m_supportsTextInput;
+    bool m_preeditActive;
+    QString m_preeditText;
+
+    QSharedPointer<NetReference> m_netReference;
+};
+
+template <int N>
+class QmlNetPaintedItem : public QmlNetPaintedItemBase
+{
+
+public:
+    QmlNetPaintedItem(QQuickItem *parent = nullptr)
+        : QmlNetPaintedItemBase(QmlNet::instantiateType(s_netTypeInfo), parent) {
+    }
+
+    virtual ~QmlNetPaintedItem() {
+    }
+
+    static void init(QSharedPointer<NetTypeInfo> info) {
+        s_netTypeInfo = info;
+    }
+
+private:
+    static QSharedPointer<NetTypeInfo> s_netTypeInfo;
+};
+
+template <int N>
+QSharedPointer<NetTypeInfo> QmlNetPaintedItem<N>::s_netTypeInfo = nullptr;
+
+#endif // QMLNETPAINTEDITEM_H
